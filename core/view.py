42b48723007921c2752070da1afc618930a4fdca
AlanPrates <61175274+AlanPrates@users.noreply.github.com>
2020-12-16 23:33:03 -0300

    up

---
+++ core/view.py
@@ -0,0 +1,23 @@
+import colorama
+
+def head():
+    print(colorama.Style.BRIGHT)
+    print(colorama.Fore.CYAN + '''
+                          '
+                        '   '  UNDEADSEC | t.me/UndeadSec 
+                      '       '  youtube.com/c/UndeadSec - BRAZIL
+                 .  '  .        '                        '
+             '             '      '                   '   '
+  ███████ ████████ ███████ ██ ███████ ██       ███████ ██ ███████ ██   ██ 
+  ██      ██    ██ ██      ██ ██   ██ ██       ██      ██ ██      ██   ██ 
+  ███████ ██    ██ ██      ██ ███████ ██       █████   ██ ███████ ███████ 
+       ██ ██    ██ ██      ██ ██   ██ ██       ██      ██      ██ ██   ██ 
+  ███████ ████████ ███████ ██ ██   ██ ███████  ██      ██ ███████ ██   ██ 
+      .    '   '....'               ..'.      ' .
+         '  .                     .     '          '     '  v3.0Nepture
+               '  .  .  .  .  . '.    .'              '  .
+                   '         '    '. '      Twitter: https://twitter.com/Alan Prates
+                     '       '      '       Site: https://www.alanprates.com
+                       ' .  '
+                           ''')
+    print(colorama.Fore.GREEN + 'Go to http://18.236.175.113:5000/neptune to start')
